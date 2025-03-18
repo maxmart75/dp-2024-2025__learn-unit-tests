4be874223a5ec08580b9752108367466972f494a
Maxence MARTIN <121853669+maxmart75@users.noreply.github.com>
2025-03-18 09:57:03 +0100

    FizzBuzz

--- src/main/java/fr/anthonyquere/fizzbuzz/FizzBuzz.java
+++ src/main/java/fr/anthonyquere/fizzbuzz/FizzBuzz.java
@@ -1,14 +1,41 @@
 package fr.anthonyquere.fizzbuzz;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FizzBuzz {
-
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        
+        List<String> result = startFizzBuzz(15);
+        System.out.println(result);
     }
 
     public static List<String> startFizzBuzz(int count) {
-        return null;
+        if (count == 0) {
+            return new ArrayList<>();
+        }
+        
+        List<String> result = new ArrayList<>();
+        
+        for (int i = 1; i <= count; i++) {
+            String element = "";
+            
+            if (i % 3 == 0) {
+                element += "Fizz";
+            }
+            
+            if (i % 5 == 0) {
+                element += "Buzz";
+            }
+            
+            if (element.isEmpty()) {
+                element = String.valueOf(i);
+            }
+            
+            result.add(element);
+        }
+        
+        return result;
     }
 }
