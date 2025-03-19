1167202057fce08e28891720bbd051632a540688
Maxence MARTIN <121853669+maxmart75@users.noreply.github.com>
2025-03-19 10:49:09 +0100

    final

--- src/test/TeaShopTest.java
+++ src/test/TeaShopTest.java
@@ -1,63 +1,211 @@
-import org.junit.jupiter.api.Test;
 package fr.anthonyquere.teashop;
 
-@Test
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+import org.mockito.Mockito;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-//test qui montre que le constructeur initialise correctement tous les attributs d'un objet Tea.
-    public void testConstructor_ValidParameters_CreatesTeaObject() {
-        Tea tea = new Tea("Matcha", 180, 95, true);
-        
-        assertEquals("Matcha", tea.getName());
-        assertEquals(180, tea.getSteepingTimeSeconds());
-        assertEquals(95, tea.getIdealTemperatureCelsius());
-        assertTrue(tea.isLoose());
+public class TeaShopTests {
+
+    public static class TeaTest {
+        @Test
+        // test qui montre si le constructeur initialise bien les attribut
+        public void testConstructor_ValidParameters_CreatesTeaObject() {
+            
+            Tea tea = new Tea("Green Tea", 180, 95, true);
+            
+            assertEquals("Green Tea", tea.getName());
+            assertEquals(180, tea.getSteepingTimeSeconds());
+            assertEquals(95, tea.getIdealTemperatureCelsius());
+            assertTrue(tea.isLoose());
+        }
     }
+    
+    public static class TeaCupTest {
+        private TeaCup teaCup;
+        
+        public void setUp() {
+            teaCup = new TeaCup();
+        }
+        
+        @Test
+        // test qui montre si l'ajout d'eau change l'état de la tasse
+        public void testAddWater_ValidTemperature_SetsTemperature() {
+            
+            teaCup.addWater(85);
+            
+            assertFalse(teaCup.isEmpty());
+        }
+        
+        @Test
+        // test si une erreur est lancé quand on ajoute du thé sans eau
+        public void testAddTea_EmptyCup_ThrowsException() {
+            
+            Tea tea = new Tea("Matcha", 240, 90, false);
+            
+            assertThrows(IllegalStateException.class, () -> teaCup.addTea(tea));
+        }
+        
+        @Test
+        // test si on peut ajouter a une tasse qui contient de l'eau
+        public void testAddTea_NonEmptyCup_AddsTea() {
+            
+            teaCup.addWater(90);
+            Tea tea = new Tea("Matcha", 240, 90, false);
+            
+            assertDoesNotThrow(() -> teaCup.addTea(tea));
+        }
+        
+        @Test
+        // test qui retourne une erreur si une tasse n'a pas de thé
+        public void testIsReadyToDrink_NoTea_ReturnsFalse() {
+            
+            teaCup.addWater(90);
+            
+            assertFalse(teaCup.isReadyToDrink());
+        }
+        
+        
+        @Test
+        // test qui retourne true si les condition sont bonnes
+        public void testIsReadyToDrink_SteepingTimeComplete_IdealTemperature_ReturnsTrue() {
+            TeaCup spyTeaCup = Mockito.spy(teaCup);
+            doReturn(1000).when(spyTeaCup).getCurrentTimeInSeconds();
+            
+            spyTeaCup.addWater(90);
+            
+            Tea tea = new Tea("Matcha", 240, 90, false);
+            spyTeaCup.addTea(tea);
+            
+            doReturn(1241).when(spyTeaCup).getCurrentTimeInSeconds();
+            
+            
+            assertTrue(spyTeaCup.isReadyToDrink());
+        }
+        
+        @Test
+        //test qui retourne false si la température est trop basse
+        public void testIsReadyToDrink_SteepingTimeComplete_TemperatureTooLow_ReturnsFalse() {
 
+            TeaCup spyTeaCup = Mockito.spy(teaCup);
+            doReturn(1000).when(spyTeaCup).getCurrentTimeInSeconds();
+            
+            spyTeaCup.addWater(80); 
+            
+            Tea tea = new Tea("Matcha", 240, 90, false);
+            spyTeaCup.addTea(tea);
+            
+            doReturn(1241).when(spyTeaCup).getCurrentTimeInSeconds();
+            
+            assertFalse(spyTeaCup.isReadyToDrink());
+        }
+        
+        @Test
+        public void testIsReadyToDrink_SteepingTimeComplete_TemperatureWithinRange_ReturnsTrue() {
 
-@Test
-//Test qui montre que la méthode setName() modifie correctement le nom du thé.
-    public void testSetName_NewName_NameIsUpdated() {
-        
-        Tea tea = new Tea("Irish cofee", 120, 80, false);
-        
-        tea.setName("Matcha");
-        
-        assertEquals("Matcha", tea.getName());
+            TeaCup spyTeaCup = Mockito.spy(teaCup);
+            doReturn(1000).when(spyTeaCup).getCurrentTimeInSeconds();
+            
+            spyTeaCup.addWater(93); 
+            
+            Tea tea = new Tea("Matcha", 240, 90, false);
+            spyTeaCup.addTea(tea);
+            
+            doReturn(1241).when(spyTeaCup).getCurrentTimeInSeconds();
+            
+            assertTrue(spyTeaCup.isReadyToDrink());
+        }
     }
+  
+    public static class TeaShopTest {
+        private TeaShop teaShop;
+        private Tea blackTea;
+        private Tea greenTea;
+        
+        public void setUp() {
+            teaShop = new TeaShop(90);
+            blackTea = new Tea("Matcha", 240, 90, false);
+            greenTea = new Tea("Irish cofee", 120, 80, true);
+        }
+        
+        @Test
+        // test qui verifie si le thé est correctement ajouter à la boutique
+        public void testAddTea_ValidTea_TeaAvailableForPreparation() {
 
-@Test
-//Test qui montre que la méthode setSteepingTimeSeconds() modifie correctement le temps d'infusion du thé.
-    public void testSetSteepingTimeSeconds_NewTime_TimeIsUpdated() {
+            teaShop.addTea(blackTea);
+            
+            TeaCup cup = teaShop.prepareTea("Matcha");
+            assertNotNull(cup);
+        }
         
-        Tea tea = new Tea("Italian cofee", 120, 80, false);
+        @Test
+        //test qui verifie que la tasse est non null si le thé est disponible
+        public void testPrepareTea_TeaAvailable_ReturnsCupWithTea() {
+            
+            teaShop.addTea(blackTea);
+            
+            TeaCup cup = teaShop.prepareTea("Matcha");
+            
+            assertNotNull(cup);
+        }
         
-        tea.setSteepingTimeSeconds(180);
+        @Test
+        // verifie si une erreur apparait en cas de non disponibilité du thé
+        public void testPrepareTea_TeaNotAvailable_ThrowsException() {
+            
+            assertThrows(IllegalArgumentException.class, () -> teaShop.prepareTea("Green Tea"));
+        }
         
-        assertEquals(180, tea.getSteepingTimeSeconds());
+        @Test
+        // test qui verifie si la fonction prepareTea fonctionne avec plusieurs thé 
+        public void testPrepareTea_MultipleTeaOptions_ReturnsCorrectTea() {
+           
+            teaShop.addTea(blackTea);
+            teaShop.addTea(greenTea);
+            
+            
+            TeaCup cup = teaShop.prepareTea("Irish cofee");
+            
+            
+            assertNotNull(cup);
+        }
+        
+        @Test
+        // test qui verifie si la temperature change bien
+        public void testSetWaterTemperature_ValidTemperature_TemperatureIsSet() {
+            
+            teaShop.setWaterTemperature(85);
+            
+            teaShop.addTea(greenTea);
+            assertDoesNotThrow(() -> teaShop.prepareTea("Irish cofee"));
+        }
+        
+        @Test
+        // test qui affiche une erreur si la temperature est négative
+        public void testSetWaterTemperature_TooLow_ThrowsException() {
+            
+            assertThrows(IllegalArgumentException.class, () -> teaShop.setWaterTemperature(-5));
+        }
+        
+        @Test
+        // test qui retourne une erreur si la temperature depasse 100
+        public void testSetWaterTemperature_TooHigh_ThrowsException() {
+            
+            assertThrows(IllegalArgumentException.class, () -> teaShop.setWaterTemperature(101));
+        }
+        
+        @Test
+        // test qui verfie sur les valer de temperature sunt respecter
+        public void testSetWaterTemperature_EdgeCases_AcceptsValues() {
+            
+            assertDoesNotThrow(() -> teaShop.setWaterTemperature(0));
+            assertDoesNotThrow(() -> teaShop.setWaterTemperature(100));
+        }
     }
+}
 
-@Test
-//Test qui montre que la méthode setIdealTemperatureCelsius() modifie correctement la température idéale
-
-    public void test_SetIdealTemperatureCelsius_NewTemperature_TemperatureIsUpdated() {
-        
-        Tea tea = new Tea("Expresso", 120, 80, false);
-        
-        tea.setIdealTemperatureCelsius(95);
-
-        assertEquals(95, tea.getIdealTemperatureCelsius());
-    }
-
-@Test
-//Test qui montre que la méthode setLoose() modifie correctement le type de thé.
-    public void testSetLoose_NewType_TypeIsUpdated() {
-        
-        Tea tea = new Tea("Expresso", 120, 80, false);
-        
-        tea.setLoose(true);
-        
-        assertTrue(tea.isLoose());
-    }
 
     
 
